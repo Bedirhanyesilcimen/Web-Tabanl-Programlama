4cbdc267d965ae1dc68da41e02403a82d8b308ce
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 14:00:42 +0300

    HomeViewModel.cs

--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -1,19 +1,14 @@
 using WorkFlowApp.Models;
 
-namespace WorkFlowApp.ViewModels // ViewModels klasörü yoksa Models içine de koyabilirsin, sorun değil.
+namespace WorkFlowApp.ViewModels 
 {
     public class HomeViewModel
     {
-        // 1. ÜSTTEKİ KUTUCUKLAR (SAYAÇLAR)
         public int BugunUretilenKoli { get; set; }
         public int ToplamDepoStogu { get; set; }
-        public int BekleyenSiparis { get; set; } // Şimdilik 0 gelecek, ileride bağlarız
+        public int BekleyenSiparis { get; set; } 
         public int PersonelSayisi { get; set; }
-
-        // 2. KRİTİK STOK LİSTESİ (Tablo)
         public List<MamulStok> KritikStoklar { get; set; } = new List<MamulStok>();
-
-        // 3. GRAFİK VERİLERİ (Son 7 Gün)
         public List<string> GrafikTarihler { get; set; } = new List<string>();
         public List<int> GrafikVeriler { get; set; } = new List<int>();
     }
