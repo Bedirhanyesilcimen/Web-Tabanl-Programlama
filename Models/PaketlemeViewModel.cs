75a12e77daa0ec2d532ba78cbeca7a6ea60083d5
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 14:03:03 +0300

    PaketlemeViewModel.cs

--- Models/PaketlemeViewModel.cs
+++ Models/PaketlemeViewModel.cs
@@ -2,10 +2,7 @@ namespace WorkFlowApp.Models
 {
     public class PaketlemeViewModel
     {
-        // Sayfanın altındaki tablo için geçmiş üretimler
         public List<PaketlemeUretim> GecmisUretimler { get; set; } = new List<PaketlemeUretim>();
-
-        // Dropdown (Seçim Kutusu) için Depodaki Ürün İsimleri
         public List<string> DepodakiUrunler { get; set; } = new List<string>();
     }
 }
