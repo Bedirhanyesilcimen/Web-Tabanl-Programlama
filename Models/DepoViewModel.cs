99e3c47d582288cd0c40e769f7cfb9b9e18eb88e
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 12:29:40 +0300

    DepoViewModel.cs

--- Models/DepoViewModel.cs
+++ Models/DepoViewModel.cs
@@ -4,16 +4,9 @@ namespace WorkFlowApp.Models
 {
     public class DepoViewModel
     {
-        // 1. Bitmiş Ürünler (Eldiven)
         public List<MamulStok> MamulStoklari { get; set; } = new List<MamulStok>();
-
-        // 2. Hammaddeler (İplik - Lotlu)
         public List<HammaddeStok> HammaddeStoklari { get; set; } = new List<HammaddeStok>();
-
-        // 3. Tekstil (Astar - Yarı Mamul) -- YENİ EKLENEN
         public List<TekstilStok> TekstilStoklari { get; set; } = new List<TekstilStok>();
-        
-        // Loglar
         public List<StokLog> SonHareketler { get; set; } = new List<StokLog>();
     }
 }
