2e2272e7e59ebeeb3aa095db4712abf7ef3f21f5
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 14:03:32 +0300

    Recete.cs

--- Models/Recete.cs
+++ Models/Recete.cs
@@ -8,14 +8,11 @@ namespace WorkFlowApp.Models
         public int Id { get; set; }
 
         [Display(Name = "Ürün Adı")]
-        public string UrunAdi { get; set; } = string.Empty; // Hangi ürünün reçetesi?
-
+        public string UrunAdi { get; set; } = string.Empty;
         [Display(Name = "Bileşen (Hammadde)")]
         public string BilesenAdi { get; set; } = string.Empty;
-
         [Display(Name = "Kullanım Miktarı")]
-        public decimal KullanimMiktari { get; set; } // Örn: 0.05
-
+        public decimal KullanimMiktari { get; set; } 
         [Display(Name = "Birim")]
         public string Birim { get; set; } = "Kg";
     }
