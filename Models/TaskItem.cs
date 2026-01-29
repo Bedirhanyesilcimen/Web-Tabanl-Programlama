e442f60e55f0dc07e53a77c829cdfda003232d0d
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 14:04:44 +0300

    TaskItem.cs

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -7,24 +7,17 @@ namespace WorkFlowApp.Models
     {
         [Key]
         public int Id { get; set; }
-
         [Required]
         [Display(Name = "Görev Başlığı")]
         public string Title { get; set; } = string.Empty;
-
         [Display(Name = "Açıklama")]
-        public string? Description { get; set; } // ? koyduk
-
+        public string? Description { get; set; }
         [Display(Name = "Durum")]
         public bool IsCompleted { get; set; }
-
         [DataType(DataType.Date)]
         [Display(Name = "Son Tarih")]
         public DateTime Deadline { get; set; }
-
-        // Görev kime atandı?
         public int? EmployeeId { get; set; }
-        
         [ForeignKey("EmployeeId")]
         public Employee? Employee { get; set; }
     }
