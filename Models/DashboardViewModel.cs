044d7b63f454c6b9ebda8c48a0daa795e007f1d4
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 11:49:42 +0300

    DashboardViewModel.cs

--- Models/DashboardViewModel.cs
+++ Models/DashboardViewModel.cs
@@ -4,18 +4,13 @@ namespace WorkFlowApp.Models
 {
     public class DashboardViewModel
     {
-        // 'string?' yaparak null olabilir dedik, uyarı gider.
         public string? KullaniciRolu { get; set; }
-
         public int ToplamPersonel { get; set; }
         public int ToplamUrunCesidi { get; set; }
-
         public int KritikStokSayisi { get; set; } 
         public int GelecekSevkiyatlar { get; set; } 
-
         public int BekleyenTalepler { get; set; } 
         public int AktifSiparisler { get; set; } 
-
         public int UretimdekiIsler { get; set; }
     }
 }
