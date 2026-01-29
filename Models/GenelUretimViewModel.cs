43af02e0637ddddf0363df6381ae227c0b5a2c77
Bedirhanyesilcimen <becine16@gmail.com>
2026-01-29 12:30:38 +0300

    GenelUretimViewModel.cs

--- Models/GenelUretimViewModel.cs
+++ Models/GenelUretimViewModel.cs
@@ -2,7 +2,6 @@ namespace WorkFlowApp.Models
 {
     public class GenelUretimViewModel
     {
-        // System.Collections.Generic... diyerek tam adres veriyoruz ki hata şansı kalmasın
         public System.Collections.Generic.List<KaplamaUretim> KaplamaListesi { get; set; } = new System.Collections.Generic.List<KaplamaUretim>();
         public System.Collections.Generic.List<TekstilUretim> TekstilListesi { get; set; } = new System.Collections.Generic.List<TekstilUretim>();
     }
